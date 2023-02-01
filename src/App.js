141221861083f62c00f52312dad86d7517500460
llite22 <llite2@yandex.ru>
2023-02-01 18:54:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
+import { useState } from "react";
+import { useEffect } from "react";
+
+// Тут список пользователей: https://reqres.in/api/users
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [invites, setInvites] = useState([]);
+  const [success, setSuccess] = useState(false);
+  
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+  
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+  
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+  
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+  
+  return (
+    <div className="App">
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
